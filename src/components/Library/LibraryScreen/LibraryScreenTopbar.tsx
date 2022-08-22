3f2a2ed48767034a43fb4aa1ce05a8fc896b611b
Bruno Mariz <bruno.mariz@semantix.inc>
2022-08-22 13:11:35 -0300

    fix: change cursor on window move

--- src/components/Library/LibraryScreen/LibraryScreenTopbar.tsx
+++ src/components/Library/LibraryScreen/LibraryScreenTopbar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useAppDispatch } from "../../../redux/app/hooks";
 import { hideLibrary } from "../../../redux/features/nodeLibrary/nodeLibrarySlice";
 import { AiFillCloseCircle, AiFillCloseSquare } from "react-icons/ai";
+import { cursorVarietyChanged } from "../../../redux/features/cursor/cursorSlice";
 
 type Props = {};
 
@@ -11,7 +12,10 @@ function LibraryScreenTopbar({}: Props) {
     <div className="border-b-2 flex justify-between items-center bg-gray-50 h-8">
       <span className="px-2">Library</span>
       <button
-        onClick={() => dispatch(hideLibrary())}
+        onClick={() => {
+          dispatch(hideLibrary());
+          dispatch(cursorVarietyChanged("Move"));
+        }}
         className="text-red-500 py-1 px-0 m-1 hover:scale-110 rounded-sm flex items-center justify-center "
       >
         <AiFillCloseSquare size={25}></AiFillCloseSquare>
